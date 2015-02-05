4e554fd8379b8404bc8bcef8836474feb0f35ab3
John Verity <jverity@jbrestaurants.com>
2015-02-05 17:24:32 -0500

    Pivoted rows for medical deductions into columns.

--- AVG Hrs PPP + Medical Deductions.sql
+++ AVG Hrs PPP + Medical Deductions.sql
@@ -2,15 +2,34 @@
 Selecting for active employees' average hours per pay period, and their active medical deductions
 */
 
-create table ##1 (employid int, trxbegdt datetime, untstopy float); 
 
-insert into ##1
+create table ##1 (employid int, trxbegdt datetime, untstopy float); --using this temp table for sum of employee's hours, one row per pay period
+CREATE TABLE ##2 (EMPLOYID INT, BUYUPF CHAR(6), BUYUPS CHAR (6), COREF CHAR(5), CORES CHAR(5), DFHR CHAR(4), DFS CHAR(3), DSHR CHAR(4), DSS CHAR(3), VFHR CHAR(4), VFS CHAR(3), VSHR CHAR(4), VSS CHAR(3)); --using this table for pivoted medical deductions per employee
+
+WITH PivotData AS	--create CTE with grouping, spread and aggregation fields; spreading and aggregation fields happen to be the same in this case - DEDUCTON
+(
+SELECT
+EMPLOYID,
+DEDUCTON
+FROM upr00500
+WHERE INACTIVE = 0
+AND DEDUCtON IN ('BUYUPF', 'BUYUPS', 'COREF', 'CORES', 'DFHR', 'DFS', 'DSHR', 'DSS', 'VFHR', 'VFS', 'VSHR', 'VSS')
+)
+
+INSERT INTO ##2
+SELECT *			--Pivot the fields from the CTE
+FROM PivotData
+PIVOT( MAX(DEDUCTON)	
+	FOR DEDUCTON IN ([BUYUPF], [BUYUPS], [COREF], [CORES], [DFHR], [DFS], [DSHR], [DSS], [VFHR], [VFS], [VSHR], [VSS]) ) AS P;
+
+
+insert into ##1		--inserting into temp table for sum of employee's hours, one row per pay period
 select a.employid, a.trxbegdt, sum(a.untstopy)
 from upr30300 a join upr00100 b on a.employid = b.employid 
 where trxbegdt > '2013-12-26' and b.inactive = 0 and untstopy > 0
 group by a.employid, a.trxbegdt
 
-select DB_NAME() [Company], b.deprtmnt, a.employid, b.jobtitle, 
+select DB_NAME() [Company], b.deprtmnt, a.employid, b.jobtitle,
 CASE 
       WHEN b.EMPLOYMENTTYPE = 1 THEN 'Full Time Regular' 
       WHEN b.EMPLOYMENTTYPE = 2 THEN 'Full Time Temp'
@@ -24,43 +43,19 @@ CASE
       WHEN b.GENDER = 1 THEN 'Male' 
       WHEN b.GENDER = 2 THEN 'Female'
    END AS Gender, 
-b.STRTDATE, avg(a.untstopy) [Avg Hours PPP], c.ZIPCODE, 'No Medical/Dental/Vision'
+b.STRTDATE, avg(a.untstopy) [Avg Hours PPP], c.ZIPCODE,
+d.BUYUPF [BuyUp Fam], d.BUYUPS [BuyUp Sng], d.COREF [Core Fam], d.CORES [Core Sng], 
+d.DFHR [Dental Fam Hrly], d.DFS [Dental Fam Salry], d.DSHR [Dental Sng Hrly], d.DSS [Dental Dng Salry], 
+d.VFHR [Vision Fam Hrly], d.VFS [Vision Fam Salry], d.VSHR [Vision Sng Hrly], d.VSS [Vision Sng Salry]
 
 from ##1 a join upr00100 b on a.employid = b.employid 
 		   JOIN UPR00102 c ON a.employid = c.EMPLOYID
-	
-WHERE b.INACTIVE = 0
-		
-group by a.employid, b.frstname, b.lastname, b.BRTHDATE, b.EMPLOYMENTTYPE, b.deprtmnt, b.jobtitle, b.GENDER, b.STRTDATE, c.ZIPCODE
+		   LEFT JOIN ##2 d ON a.employid = d.EMPLOYID	--using left join because not all active employees in ##1 are in ##2 (only empl. with med deds. are in ##2)
 
-UNION ALL
+--have to put all fields in 'group by' clause b/c using avg() on units to pay field; if one field gets aggregated, all fields get aggregated		
+group by a.employid, b.frstname, b.lastname, b.BRTHDATE, b.EMPLOYMENTTYPE, b.deprtmnt, b.jobtitle, b.GENDER, b.STRTDATE, c.ZIPCODE,
+d.BUYUPF, d.BUYUPS, d.COREF, d.CORES, d.DFHR, d.DFS, d.DSHR, d.DSS, d.VFHR, d.VFS, d.VSHR, d.VSS
 
-SELECT DB_NAME(), b.DEPRTMNT, a.EMPLOYID, b.JOBTITLE, 
-CASE 
-      WHEN b.EMPLOYMENTTYPE = 1 THEN 'Full Time Regular' 
-      WHEN b.EMPLOYMENTTYPE = 2 THEN 'Full Time Temp'
-	  WHEN b.EMPLOYMENTTYPE = 3 THEN 'Part Time Regular'
-	  WHEN b.EMPLOYMENTTYPE = 4 THEN 'Part Time Temp'
-	  WHEN b.EMPLOYMENTTYPE = 5 THEN 'Intern'
-	  WHEN b.EMPLOYMENTTYPE = 6 THEN 'Other' 
-   END AS [Employment Type], 
-b.FRSTNAME, b.LASTNAME, b.BRTHDATE, 
-CASE 
-      WHEN b.GENDER = 1 THEN 'Male' 
-      WHEN b.GENDER = 2 THEN 'Female'
-   END AS Gender, 
-b.STRTDATE, AVG(c.untstopy), d.ZIPCODE, a.DEDUCTON
-
-FROM UPR00500 a JOIN UPR00100 b ON a.EMPLOYID = b.EMPLOYID
-				JOIN ##1 c ON a.EMPLOYID = c.employid
-				JOIN UPR00102 d ON a.EMPLOYID = d.EMPLOYID
-
-WHERE a.DEDUCTON IN ('BUYUPF', 'BUYUPS', 'COREF', 'CORES', 'DFHR', 'DFS', 'DSHR', 'DSS', 'VFHR', 'VFS', 'VSHR', 'VSS')
-AND a.INACTIVE = 0
-AND b.INACTIVE = 0
-
-GROUP BY b.DEPRTMNT, a.EMPLOYID, b.JOBTITLE, b.EMPLOYMENTTYPE, b.FRSTNAME, b.LASTNAME, b.BRTHDATE, b.GENDER, b.STRTDATE, d.ZIPCODE, a.DEDUCTON
-
-ORDER BY a.employid
-
+--cleanup, dropping temporary tables
 drop table ##1
+drop table ##2
