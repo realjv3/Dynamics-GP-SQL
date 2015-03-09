0d4f87567dc108a6aa9c581f68ed38f610632f97
John Verity <jverity@jbrestaurants.com>
2015-03-09 16:37:54 -0400

    Added PM Historical & Open trx queries

---
+++ Multi-company History PM Trx.sql
@@ -0,0 +1,151 @@
+SELECT db_name(DB_ID('FRI')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name],
+CASE
+	WHEN DOCTYPE = 1 THEN 'Invoice'
+	WHEN DOCTYPE = 2 THEN 'Finance Charge'
+	WHEN DOCTYPE = 3 THEN 'Misc Charge'
+	WHEN DOCTYPE = 4 THEN 'Return'
+	WHEN DOCTYPE = 5 THEN 'Credit Memo'
+	WHEN DOCTYPE = 6 THEN 'Payment'
+END [Document Type],
+VCHRNMBR [Voucher Number], BACHNUMB [Batch Number], CONVERT(DATE, DOCDATE, 101) [Document Date], 
+CONVERT(DATE, DUEDATE, 101) [Due Date], CONVERT(DATE, POSTEDDT, 1) [Posted Date], CONVERT(DATE, PSTGDATE, 101) [Posting Date], DOCNUMBR [Document Number], a.TRXDSCRN Description,
+CONVERT(MONEY, DOCAMNT, 1) [Document Amount], CONVERT(MONEY, PRCHAMNT, 1) [Purchase Amount]
+FROM FRI.dbo.PM30200 a JOIN FRI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+WHERE POSTEDDT > '03-01-2015'		--select Sunday before last for lookback period 
+
+UNION ALL
+
+SELECT db_name(DB_ID('FRII')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+CASE
+	WHEN DOCTYPE = 1 THEN 'Invoice'
+	WHEN DOCTYPE = 2 THEN 'Finance Charge'
+	WHEN DOCTYPE = 3 THEN 'Misc Charge'
+	WHEN DOCTYPE = 4 THEN 'Return'
+	WHEN DOCTYPE = 5 THEN 'Credit Memo'
+	WHEN DOCTYPE = 6 THEN 'Payment'
+END [Document Type],
+VCHRNMBR [Voucher Number], BACHNUMB [Batch Number], CONVERT(DATE, DOCDATE, 101) [Document Date], 
+CONVERT(DATE, DUEDATE, 101) [Due Date], CONVERT(DATE, POSTEDDT, 1) [Posted Date], CONVERT(DATE, PSTGDATE, 101) [Posting Date], DOCNUMBR [Document Number], a.TRXDSCRN Description,
+CONVERT(MONEY, DOCAMNT, 1) [Document Amount], CONVERT(MONEY, PRCHAMNT, 1) [Purchase Amount]
+FROM FRII.dbo.PM30200 a JOIN FRI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+WHERE POSTEDDT > '03-01-2015'		--select Sunday before last for lookback period 
+ 
+UNION ALL
+
+SELECT db_name(DB_ID('FRNJ')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+CASE
+	WHEN DOCTYPE = 1 THEN 'Invoice'
+	WHEN DOCTYPE = 2 THEN 'Finance Charge'
+	WHEN DOCTYPE = 3 THEN 'Misc Charge'
+	WHEN DOCTYPE = 4 THEN 'Return'
+	WHEN DOCTYPE = 5 THEN 'Credit Memo'
+	WHEN DOCTYPE = 6 THEN 'Payment'
+END [Document Type],
+VCHRNMBR [Voucher Number], BACHNUMB [Batch Number], CONVERT(DATE, DOCDATE, 101) [Document Date], 
+CONVERT(DATE, DUEDATE, 101) [Due Date], CONVERT(DATE, POSTEDDT, 1) [Posted Date], CONVERT(DATE, PSTGDATE, 101) [Posting Date], DOCNUMBR [Document Number], a.TRXDSCRN Description,
+CONVERT(MONEY, DOCAMNT, 1) [Document Amount], CONVERT(MONEY, PRCHAMNT, 1) [Purchase Amount]
+FROM FRNJ.dbo.PM30200 a JOIN FRI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+WHERE POSTEDDT > '03-01-2015'		--select Sunday before last for lookback period 
+
+UNION ALL
+
+SELECT db_name(DB_ID('MASPK')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+CASE
+	WHEN DOCTYPE = 1 THEN 'Invoice'
+	WHEN DOCTYPE = 2 THEN 'Finance Charge'
+	WHEN DOCTYPE = 3 THEN 'Misc Charge'
+	WHEN DOCTYPE = 4 THEN 'Return'
+	WHEN DOCTYPE = 5 THEN 'Credit Memo'
+	WHEN DOCTYPE = 6 THEN 'Payment'
+END [Document Type],
+VCHRNMBR [Voucher Number], BACHNUMB [Batch Number], CONVERT(DATE, DOCDATE, 101) [Document Date], 
+CONVERT(DATE, DUEDATE, 101) [Due Date], CONVERT(DATE, POSTEDDT, 1) [Posted Date], CONVERT(DATE, PSTGDATE, 101) [Posting Date], DOCNUMBR [Document Number], a.TRXDSCRN Description,
+CONVERT(MONEY, DOCAMNT, 1) [Document Amount], CONVERT(MONEY, PRCHAMNT, 1) [Purchase Amount]
+FROM MASPK.dbo.PM30200 a JOIN FRI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+WHERE POSTEDDT > '03-01-2015'		--select Sunday before last for lookback period 
+
+UNION ALL
+
+SELECT db_name(DB_ID('MGMT')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+CASE
+	WHEN DOCTYPE = 1 THEN 'Invoice'
+	WHEN DOCTYPE = 2 THEN 'Finance Charge'
+	WHEN DOCTYPE = 3 THEN 'Misc Charge'
+	WHEN DOCTYPE = 4 THEN 'Return'
+	WHEN DOCTYPE = 5 THEN 'Credit Memo'
+	WHEN DOCTYPE = 6 THEN 'Payment'
+END [Document Type],
+VCHRNMBR [Voucher Number], BACHNUMB [Batch Number], CONVERT(DATE, DOCDATE, 101) [Document Date], 
+CONVERT(DATE, DUEDATE, 101) [Due Date], CONVERT(DATE, POSTEDDT, 1) [Posted Date], CONVERT(DATE, PSTGDATE, 101) [Posting Date], DOCNUMBR [Document Number], a.TRXDSCRN Description,
+CONVERT(MONEY, DOCAMNT, 1) [Document Amount], CONVERT(MONEY, PRCHAMNT, 1) [Purchase Amount]
+FROM MGMT.dbo.PM30200 a JOIN FRI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+WHERE POSTEDDT > '03-01-2015'		--select Sunday before last for lookback period 
+UNION ALL
+
+SELECT db_name(DB_ID('MI')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+CASE
+	WHEN DOCTYPE = 1 THEN 'Invoice'
+	WHEN DOCTYPE = 2 THEN 'Finance Charge'
+	WHEN DOCTYPE = 3 THEN 'Misc Charge'
+	WHEN DOCTYPE = 4 THEN 'Return'
+	WHEN DOCTYPE = 5 THEN 'Credit Memo'
+	WHEN DOCTYPE = 6 THEN 'Payment'
+END [Document Type],
+VCHRNMBR [Voucher Number], BACHNUMB [Batch Number], CONVERT(DATE, DOCDATE, 101) [Document Date], 
+CONVERT(DATE, DUEDATE, 101) [Due Date], CONVERT(DATE, POSTEDDT, 1) [Posted Date], CONVERT(DATE, PSTGDATE, 101) [Posting Date], DOCNUMBR [Document Number], a.TRXDSCRN Description,
+CONVERT(MONEY, DOCAMNT, 1) [Document Amount], CONVERT(MONEY, PRCHAMNT, 1) [Purchase Amount]
+FROM MI.dbo.PM30200 a JOIN FRI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+WHERE POSTEDDT > '03-01-2015'		--select Sunday before last for lookback period 
+
+UNION ALL
+
+SELECT db_name(DB_ID('SHIRL')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+CASE
+	WHEN DOCTYPE = 1 THEN 'Invoice'
+	WHEN DOCTYPE = 2 THEN 'Finance Charge'
+	WHEN DOCTYPE = 3 THEN 'Misc Charge'
+	WHEN DOCTYPE = 4 THEN 'Return'
+	WHEN DOCTYPE = 5 THEN 'Credit Memo'
+	WHEN DOCTYPE = 6 THEN 'Payment'
+END [Document Type],
+VCHRNMBR [Voucher Number], BACHNUMB [Batch Number], CONVERT(DATE, DOCDATE, 101) [Document Date], 
+CONVERT(DATE, DUEDATE, 101) [Due Date], CONVERT(DATE, POSTEDDT, 1) [Posted Date], CONVERT(DATE, PSTGDATE, 101) [Posting Date], DOCNUMBR [Document Number], a.TRXDSCRN Description,
+CONVERT(MONEY, DOCAMNT, 1) [Document Amount], CONVERT(MONEY, PRCHAMNT, 1) [Purchase Amount]
+FROM SHIRL.dbo.PM30200 a JOIN FRI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+WHERE POSTEDDT > '03-01-2015'		--select Sunday before last for lookback period 
+
+UNION ALL
+
+SELECT db_name(DB_ID('REI')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+CASE
+	WHEN DOCTYPE = 1 THEN 'Invoice'
+	WHEN DOCTYPE = 2 THEN 'Finance Charge'
+	WHEN DOCTYPE = 3 THEN 'Misc Charge'
+	WHEN DOCTYPE = 4 THEN 'Return'
+	WHEN DOCTYPE = 5 THEN 'Credit Memo'
+	WHEN DOCTYPE = 6 THEN 'Payment'
+END [Document Type],
+VCHRNMBR [Voucher Number], BACHNUMB [Batch Number], CONVERT(DATE, DOCDATE, 101) [Document Date], 
+CONVERT(DATE, DUEDATE, 101) [Due Date], CONVERT(DATE, POSTEDDT, 1) [Posted Date], CONVERT(DATE, PSTGDATE, 101) [Posting Date], DOCNUMBR [Document Number], a.TRXDSCRN Description,
+CONVERT(MONEY, DOCAMNT, 1) [Document Amount], CONVERT(MONEY, PRCHAMNT, 1) [Purchase Amount]
+FROM REI.dbo.PM30200 a JOIN FRI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+WHERE POSTEDDT > '03-01-2015'		--select Sunday before last for lookback period 
+
+UNION ALL
+
+SELECT db_name(DB_ID('REII')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+CASE
+	WHEN DOCTYPE = 1 THEN 'Invoice'
+	WHEN DOCTYPE = 2 THEN 'Finance Charge'
+	WHEN DOCTYPE = 3 THEN 'Misc Charge'
+	WHEN DOCTYPE = 4 THEN 'Return'
+	WHEN DOCTYPE = 5 THEN 'Credit Memo'
+	WHEN DOCTYPE = 6 THEN 'Payment'
+END [Document Type],
+VCHRNMBR [Voucher Number], BACHNUMB [Batch Number], CONVERT(DATE, DOCDATE, 101) [Document Date], 
+CONVERT(DATE, DUEDATE, 101) [Due Date], CONVERT(DATE, POSTEDDT, 1) [Posted Date], CONVERT(DATE, PSTGDATE, 101) [Posting Date], DOCNUMBR [Document Number], a.TRXDSCRN Description,
+CONVERT(MONEY, DOCAMNT, 1) [Document Amount], CONVERT(MONEY, PRCHAMNT, 1) [Purchase Amount]
+FROM REII.dbo.PM30200 a JOIN FRI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+WHERE POSTEDDT > '03-01-2015'		--select Sunday before last for lookback period 
+
+ORDER BY a.VENDORID, [Document Date]
--- Multi-company Work PM Trx.sql
+++ Multi-company Work PM Trx.sql
@@ -1,4 +1,4 @@
-SELECT db_name(DB_ID('FRI')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+SELECT db_name(DB_ID('FRI')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name],  
 CASE
 	WHEN DOCTYPE = 1 THEN 'Invoice'
 	WHEN DOCTYPE = 2 THEN 'Finance Charge'
@@ -9,11 +9,11 @@ CASE
 END [Document Type],
 VCHRNMBR [Voucher Number], BACHNUMB [Batch Number], CONVERT(DATE, DOCDATE, 101) [Document Date], 
 CONVERT(DATE, DUEDATE, 101) [Due Date], DOCNUMBR [Document Number], a.TRXDSCRN Description, CONVERT(MONEY, DOCAMNT, 1) [Document Amount]
-FROM FRI.dbo.PM20000 a JOIN FRI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+FROM FRI.dbo.PM10000 a JOIN FRI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
 
 UNION ALL
 
-SELECT db_name(DB_ID('FRII')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+SELECT db_name(DB_ID('FRII')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name],  
 CASE
 	WHEN DOCTYPE = 1 THEN 'Invoice'
 	WHEN DOCTYPE = 2 THEN 'Finance Charge'
@@ -24,11 +24,11 @@ CASE
 END [Document Type],
 VCHRNMBR [Voucher Number], BACHNUMB [Batch Number],CONVERT(DATE, DOCDATE, 101), CONVERT(DATE, DUEDATE, 101) [Due Date], 
 DOCNUMBR [Document Number], a.TRXDSCRN Description, CONVERT(MONEY, DOCAMNT, 1) [Document Amount]
-FROM FRII.dbo.PM20000 a JOIN FRII.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+FROM FRII.dbo.PM10000 a JOIN FRII.dbo.PM00200 b ON a.VENDORID = b.VENDORID
 
 UNION ALL
 
-SELECT db_name(DB_ID('FRNJ')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+SELECT db_name(DB_ID('FRNJ')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name],  
 CASE
 	WHEN DOCTYPE = 1 THEN 'Invoice'
 	WHEN DOCTYPE = 2 THEN 'Finance Charge'
@@ -39,11 +39,11 @@ CASE
 END [Document Type],
 VCHRNMBR [Voucher Number], BACHNUMB [Batch Number],CONVERT(DATE, DOCDATE, 101), CONVERT(DATE, DUEDATE, 101) [Due Date], 
 DOCNUMBR [Document Number], a.TRXDSCRN Description, CONVERT(MONEY, DOCAMNT, 1) [Document Amount]
-FROM FRNJ.dbo.PM20000 a JOIN FRNJ.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+FROM FRNJ.dbo.PM10000 a JOIN FRNJ.dbo.PM00200 b ON a.VENDORID = b.VENDORID
 
 UNION ALL
 
-SELECT db_name(DB_ID('MASPK')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+SELECT db_name(DB_ID('MASPK')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name],  
 CASE
 	WHEN DOCTYPE = 1 THEN 'Invoice'
 	WHEN DOCTYPE = 2 THEN 'Finance Charge'
@@ -54,11 +54,11 @@ CASE
 END [Document Type],
 VCHRNMBR [Voucher Number], BACHNUMB [Batch Number],CONVERT(DATE, DOCDATE, 101), CONVERT(DATE, DUEDATE, 101) [Due Date], 
 DOCNUMBR [Document Number], a.TRXDSCRN Description, CONVERT(MONEY, DOCAMNT, 1) [Document Amount]
-FROM MASPK.dbo.PM20000 a JOIN MASPK.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+FROM MASPK.dbo.PM10000 a JOIN MASPK.dbo.PM00200 b ON a.VENDORID = b.VENDORID
 
 UNION ALL
 
-SELECT db_name(DB_ID('MGMT')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+SELECT db_name(DB_ID('MGMT')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name],  
 CASE
 	WHEN DOCTYPE = 1 THEN 'Invoice'
 	WHEN DOCTYPE = 2 THEN 'Finance Charge'
@@ -69,11 +69,11 @@ CASE
 END [Document Type],
 VCHRNMBR [Voucher Number], BACHNUMB [Batch Number],CONVERT(DATE, DOCDATE, 101), CONVERT(DATE, DUEDATE, 101) [Due Date], 
 DOCNUMBR [Document Number], a.TRXDSCRN Description, CONVERT(MONEY, DOCAMNT, 1) [Document Amount]
-FROM MGMT.dbo.PM20000 a JOIN MGMT.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+FROM MGMT.dbo.PM10000 a JOIN MGMT.dbo.PM00200 b ON a.VENDORID = b.VENDORID
 
 UNION ALL
 
-SELECT db_name(DB_ID('MI')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+SELECT db_name(DB_ID('MI')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name],  
 CASE
 	WHEN DOCTYPE = 1 THEN 'Invoice'
 	WHEN DOCTYPE = 2 THEN 'Finance Charge'
@@ -84,11 +84,11 @@ CASE
 END [Document Type],
 VCHRNMBR [Voucher Number], BACHNUMB [Batch Number],CONVERT(DATE, DOCDATE, 101), CONVERT(DATE, DUEDATE, 101) [Due Date], 
 DOCNUMBR [Document Number], a.TRXDSCRN Description, CONVERT(MONEY, DOCAMNT, 1) [Document Amount]
-FROM MI.dbo.PM20000 a JOIN MI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+FROM MI.dbo.PM10000 a JOIN MI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
 
 UNION ALL
 
-SELECT db_name(DB_ID('SHIRL')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+SELECT db_name(DB_ID('SHIRL')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name],  
 CASE
 	WHEN DOCTYPE = 1 THEN 'Invoice'
 	WHEN DOCTYPE = 2 THEN 'Finance Charge'
@@ -99,11 +99,11 @@ CASE
 END [Document Type],
 VCHRNMBR [Voucher Number], BACHNUMB [Batch Number],CONVERT(DATE, DOCDATE, 101), CONVERT(DATE, DUEDATE, 101) [Due Date], 
 DOCNUMBR [Document Number], a.TRXDSCRN Description, CONVERT(MONEY, DOCAMNT, 1) [Document Amount]
-FROM SHIRL.dbo.PM20000 a JOIN SHIRL.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+FROM SHIRL.dbo.PM10000 a JOIN SHIRL.dbo.PM00200 b ON a.VENDORID = b.VENDORID
 
 UNION ALL
 
-SELECT db_name(DB_ID('REI')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+SELECT db_name(DB_ID('REI')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name],  
 CASE
 	WHEN DOCTYPE = 1 THEN 'Invoice'
 	WHEN DOCTYPE = 2 THEN 'Finance Charge'
@@ -114,11 +114,11 @@ CASE
 END [Document Type],
 VCHRNMBR [Voucher Number], BACHNUMB [Batch Number],CONVERT(DATE, DOCDATE, 101), CONVERT(DATE, DUEDATE, 101) [Due Date], 
 DOCNUMBR [Document Number], a.TRXDSCRN Description, CONVERT(MONEY, DOCAMNT, 1) [Document Amount]
-FROM REI.dbo.PM20000 a JOIN REI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+FROM REI.dbo.PM10000 a JOIN REI.dbo.PM00200 b ON a.VENDORID = b.VENDORID
 
 UNION ALL
 
-SELECT db_name(DB_ID('REII')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name], 
+SELECT db_name(DB_ID('REII')) Company, a.VENDORID [Vendor ID], b.VENDNAME [Vendor Name],  
 CASE
 	WHEN DOCTYPE = 1 THEN 'Invoice'
 	WHEN DOCTYPE = 2 THEN 'Finance Charge'
@@ -129,6 +129,6 @@ CASE
 END [Document Type],
 VCHRNMBR [Voucher Number], BACHNUMB [Batch Number],CONVERT(DATE, DOCDATE, 101), CONVERT(DATE, DUEDATE, 101) [Due Date], 
 DOCNUMBR [Document Number], a.TRXDSCRN Description, CONVERT(MONEY, DOCAMNT, 1) [Document Amount]
-FROM REII.dbo.PM20000 a JOIN REII.dbo.PM00200 b ON a.VENDORID = b.VENDORID
+FROM REII.dbo.PM10000 a JOIN REII.dbo.PM00200 b ON a.VENDORID = b.VENDORID
 
 ORDER BY a.VENDORID, [Document Date]
